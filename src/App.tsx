be0dd492b53a29a28031065d500b0325a1a00f2b
wolf-jozef <jozef.wolf@capgemini.com>
2023-04-13 14:34:35 +0200

    Authentication with dynamics using MSAL

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+} from "@azure/msal-react";
+import { SignInButton } from "./components/SignInButton";
+import { SignOutButton } from "./components/SignOutButton";
+import PageLayout from "./components/PageLayout";
 
+// Remember that MsalProvider must be rendered somewhere higher up in the component tree
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <AuthenticatedTemplate>
+        <p>This will only render if a user is signed-in.</p>
+        <SignOutButton />
+        <PageLayout />
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <p>This will only render if a user is not signed-in.</p>
+        <SignInButton />
+      </UnauthenticatedTemplate>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./auth/authConfig";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// create PublicClientApplication instance
+const publicClientApplication = new PublicClientApplication(msalConfig);
+
+// Wrap your app component tree in the MsalProvider component
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MsalProvider instance={publicClientApplication}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
