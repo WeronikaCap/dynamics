33bfc4f20deea1070c3a5c9c123b51293674934c
vomcat <koczfix@gmail.com>
2023-05-09 11:15:26 +0200

    Add Link in nav elements and changes styles

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -3,14 +3,14 @@ import {Link} from "react-router-dom"
 import Logo from "../../assets/Logo.png"
 
 const Navigation =()=>{
-    return <nav className="flex flex-col items-center lg:flex-row lg:h-[86px] gap-[64px]">
+    return <nav className="flex flex-col items-center bg-primary lg:flex-row lg:h-[86px] gap-[64px]">
         <img className="w-[143px]" src={Logo} alt="Logo" />
         <div className="flex lg:justify-between lg:flex- grow">
             <ul className="flex flex-col lg:flex-row gap-[32px]">
-                <li>Home</li>
-                <li>Knowledge Base</li>
-                <li>Forums</li>
-                <li>My Support</li>
+                <li><Link className="hover:text-base-black" to="/">Home</Link></li>
+                <li><Link className="hover:text-base-black" to="/">Knowledge Base</Link></li>
+                <li><Link className="hover:text-base-black" to="/">Forums</Link></li>
+                <li><Link className="hover:text-base-black" to="/">My Support</Link></li>
             </ul>
             <div className="flex flex-col  lg:flex-row gap-[24px]">
                 <Link to="/">Register</Link>
