849f2110ff0dbd181d56640ab8eb97bd43e9f505
WeronikaCap <weronika.koczwara@capgemini.com>
2023-05-09 12:19:54 +0200

    Change px in styles to rem

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -6,16 +6,16 @@ import Logo from "../../assets/Logo.png"
 
 const Navigation =()=>{
     return <nav className=" bg-primary">
-        <ContentContainer className="flex flex-col items-center lg:flex-row gap-[64px] lg:h-[86px] ">
-            <img className="w-[143px]" src={Logo} alt="Logo" />
+        <ContentContainer className="flex flex-col items-center lg:flex-row gap-16 lg:h-[86px] ">
+            <img className="w-100" src={Logo} alt="Logo" />
             <div className="flex lg:justify-between lg:flex- grow">
-                <ul className="flex flex-col lg:flex-row gap-[32px]">
+                <ul className="flex flex-col lg:flex-row gap-8">
                     <li><Link className="hover:text-base-black" to="/">Home</Link></li>
                     <li><Link className="hover:text-base-black" to="/">Knowledge Base</Link></li>
                     <li><Link className="hover:text-base-black" to="/">Forums</Link></li>
                     <li><Link className="hover:text-base-black" to="/">My Support</Link></li>
                 </ul>
-                <div className="flex flex-col  lg:flex-row gap-[24px]">
+                <div className="flex flex-col  lg:flex-row gap-6">
                     <Link to="/">Register</Link>
                     <Link to="/">Log in</Link>
                 </div>
