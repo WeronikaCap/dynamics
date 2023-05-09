d80d3f5491bd77444c20c0c941c32b9b72cb2a37
vomcat <koczfix@gmail.com>
2023-05-09 10:54:25 +0200

    Change styles in navigation

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -3,18 +3,21 @@ import {Link} from "react-router-dom"
 import Logo from "../../assets/Logo.png"
 
 const Navigation =()=>{
-    return <nav className="flex flex-col items-center lg:justify-between lg:flex-row lg:h-[86px]">
+    return <nav className="flex flex-col items-center lg:flex-row lg:h-[86px] gap-[64px]">
         <img className="w-[143px]" src={Logo} alt="Logo" />
-        <ul className="flex flex-col lg:flex-row gap-[32px]">
-            <li>Home</li>
-            <li>Knowledge Base</li>
-            <li>Forums</li>
-            <li>My Support</li>
-        </ul>
-        <div className="flex flex-col  lg:flex-row gap-[24px]">
-            <Link to="/">Register</Link>
-            <Link to="/">Log in</Link>
+        <div className="flex lg:justify-between lg:flex- grow">
+            <ul className="flex flex-col lg:flex-row gap-[32px]">
+                <li>Home</li>
+                <li>Knowledge Base</li>
+                <li>Forums</li>
+                <li>My Support</li>
+            </ul>
+            <div className="flex flex-col  lg:flex-row gap-[24px]">
+                <Link to="/">Register</Link>
+                <Link to="/">Log in</Link>
+            </div>
         </div>
+
     </nav>
 }
 
