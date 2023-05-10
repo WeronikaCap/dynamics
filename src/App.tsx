10f5e0184e2e0fa7956447eb0e7c589329e7e200
wolf-jozef <jozef.wolf@capgemini.com>
2023-05-10 10:52:18 +0200

    conflict resolved

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,26 @@ import { SignOutButton } from "./components/SignOutButton";
 
 import Navigation from "./components/Layout/Navigation";
 import PageLayout from "./components/PageLayout";
-<<<<<<< HEAD
 import ArticleSection from "./components/articleSection/ArticleSection";
-=======
 import Footer from "./components/Footer";
->>>>>>> develop
 
 // Remember that MsalProvider must be rendered somewhere higher up in the component tree
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="w-screen">
-=======
     <>
-    <Navigation/>
->>>>>>> develop
+      <Navigation />
       <AuthenticatedTemplate>
-        <p>This will only render if a user is signed-in.</p>
+        This will only render if a user is signed-in.
         <SignOutButton />
         <PageLayout />
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
-        <p>This will only render if a user is not signed-in.</p>
+        This will only render if a user is not signed-in.
         <SignInButton />
       </UnauthenticatedTemplate>
-<<<<<<< HEAD
       <ArticleSection />
-    </div>
-=======
       <Footer />
     </>
->>>>>>> develop
   );
 }
 
