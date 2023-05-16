f54509bcca49197bc018c2ddb73233cf8a1304d6
WeronikaCap <weronika.koczwara@capgemini.com>
2023-05-16 09:52:25 +0200

    Add knowledgebase route & change global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Homepage from "pages/Homepage";
+import KnowledgeBase from "pages/KnowledgeBase";
 
-// Remember that MsalProvider must be rendered somewhere higher up in the component tree
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/knowledge-base" element={<KnowledgeBase />} />
       </Routes>
   );
 }
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -25,8 +25,8 @@ const Header =(props: HeaderProps) => {
     return (
         <header style={{'--image-url': `url(${backgroundImage})`}} className={`bg-[image:var(--image-url)] bg-cover bg-no-repeat ${paddingSmall ? "pt-12 pb-12" : "pt-32 pb-32"}`}>
             <div className={`w-[760px] m-auto flex flex-col items-center`}>
-                <h1 className="pb-4 text-neutral-black font-bold">{title}</h1>
-                {description && <p className="pb-12">{description}</p>}
+                <h1 className="pb-4">{title}</h1>
+                {description && <p className="pb-12 text-center">{description}</p>}
                 <Paper component="form" sx={{display: 'flex', alignItems: 'center', width: 400 }}>
                     <InputBase sx={{ ml: 1, flex: 1 }} placeholder={placeHolderText} inputProps={{ 'aria-label': placeHolderText }} />
                     <IconButton type="button" sx={[{ p: '10px', bgcolor:"#437EF7", borderRadius: 1 }, {'&:hover': {backgroundColor: 'red',}},]} aria-label="search" >
