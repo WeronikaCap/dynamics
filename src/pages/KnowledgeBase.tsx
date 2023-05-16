52fd7bfe38581be0d324b2cb8560b3656b20de76
WeronikaCap <weronika.koczwara@capgemini.com>
2023-05-16 09:51:44 +0200

    Add knowledgebase page

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -1,8 +1,13 @@
-import KnowledgeBaseSection from "../components/KnowledgeBase/KnowledgeBaseItem";
+import Header from "components/Layout/Header";
+import KnowledgeBaseSection from "components/KnowledgeBase/KnowledgeBaseItem";
 
 const KnowledgeBase = () =>{
     return (
-        <KnowledgeBaseSection/>
+        <>
+            <Header placeHolderText="Search the knowledge base..." title="Customer Self-Service" description="The Knowledge Base contains numerous support references, created by our support professionals who have resolved issues for our customers. It is constantly updated, expanded, and refined to ensure that you have access to the very latest information." 
+            backgroundImage="/src/assets/Hero-BG.png"/>
+            <KnowledgeBaseSection/>
+        </>
     )
 }
 
