b3c6de67820d1c4415ab40a130148f8e81b726b3
sacktomasz <120012324+sacktomasz@users.noreply.github.com>
2023-06-01 12:12:37 +0200

    deleted unused import

--- src/components/articleSection/ArticleSection.tsx
+++ src/components/articleSection/ArticleSection.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useIsAuthenticated } from "@azure/msal-react";
 import { useArticleService } from "service/ArticleService";
 import ArticleSlider from "./ArticleSlider";
